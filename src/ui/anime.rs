570e921afeb18f644d51620fdb0031a1e319ceeb
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-06-09 15:10:41 +0500

    hardcoded select prototyping or smthn

--- src/ui/anime.rs
+++ src/ui/anime.rs
@@ -12,11 +12,37 @@ pub fn render_panel<B: Backend>(f: &mut Frame<B>, area: Rect){
         ])
         .split(area);
     
+    let anime_list : Vec<ListItem> = vec![
+
+        ListItem::new("hello"),
+        ListItem::new("hello1"),
+        ListItem::new("hell2"),
+        ListItem::new("hellr").style(Style::default().bg(Color::Red)),
+        ListItem::new("hell4"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+        ListItem::new("hell5"),
+    ];
     // let widget = Paragraph::new("hello").style(Style::default().fg(Color::Cyan));
     let list = Block::default().title("list").borders(Borders::ALL);
+    let actual_list = List::new(anime_list);
     let desc = Block::default().title("desc").borders(Borders::ALL);
 
     f.render_widget(list, chunks[0]);
+    let margin_container = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(100)])
+        .margin(1)
+        .split(chunks[0]);
+    f.render_widget(actual_list, margin_container[0]);
     f.render_widget(desc, chunks[2]);
 }
 
